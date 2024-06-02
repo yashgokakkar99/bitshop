6a1de70b94f68863c6100326db5b9ef83597ff12
Yash Gokakkar <yashgokakkar@gmail.com>
2024-06-02 13:07:55 +0530

    Project pre-development setup complete

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log(`Successfully connected to mongoDB 😎`);
+
+    } catch(error) {
+        console.log(`ERROR : ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
