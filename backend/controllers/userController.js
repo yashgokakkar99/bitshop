b21ea82c9f77be55518d29605b32184b1e6fb48c
Yash Gokakkar <yashgokakkar@gmail.com>
2024-06-02 23:01:43 +0530

    Create new user and password encryption task completed

---
+++ backend/controllers/userController.js
@@ -0,0 +1,36 @@
+import User from "../models/userModel.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import bcrypt from "bcryptjs";
+
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    throw new Error("Please fill all the details 🤷‍♀️");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400).send("User already exists");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      password: newUser.password,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+export { createUser };
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,23 @@
 // packages
-
-import path from 'path';
+// import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import userRoutes from '../backend/routes/userRoutes.js'
+
 
 // utils
 
 import connectDB from './config/db.js';
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 connectDB()
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
-app.use(cookieParser)
+app.use(cookieParser())
 
-app.get('/',(req,res) => {
-    res.send("Hello World");
-})
+app.use("/api/users",userRoutes);
 
 app.listen(port, ()=>{
     console.log(`Bro server running on port: ${port} 👋`)
