c76ef624dcdf924d06e83d79a910c6996f1d2cc2
Yash Gokakkar <yashgokakkar@gmail.com>
2024-06-03 11:18:03 +0530

    Login and logout functionality with jwt implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import bcrypt from "bcryptjs";
+import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
@@ -20,6 +21,8 @@ const createUser = asyncHandler(async (req, res) => {
 
   try {
     await newUser.save();
+    createToken(res, newUser._id);
+
     res.status(201).json({
       _id: newUser._id,
       username: newUser.username,
@@ -33,4 +36,31 @@ const createUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { createUser };
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (isPasswordValid) {
+      createToken(res, existingUser._id);
+      res.status(201).json({
+        _id: existingUser._id,
+        username: existingUser.username,
+        email: existingUser.email,
+        password: existingUser.password,
+        isAdmin: existingUser.isAdmin,
+      });
+      return; //Exiting the function after sending the response
+    }
+  }
+});
+
+const logoutCurrentUser = asyncHandler(async(req,res)=>{
+  res.cookie('jwt','',{httpOnly:true,expires:new Date(0)})
+  res.status(200).json({message:"logout successfully"});
+})
+
+export { createUser, loginUser, logoutCurrentUser };
